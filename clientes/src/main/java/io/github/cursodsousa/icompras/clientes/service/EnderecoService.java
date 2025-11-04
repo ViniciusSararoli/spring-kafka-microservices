d02f81fefbf88aa272bef866a624a4d323a07513
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-11-04 00:53:40 -0300

    Adicionando controlador, serviço, modelo e repositório para adicionar, consultar enredeço pelo id do endereço e pelo id do cliente

---
+++ clientes/src/main/java/io/github/cursodsousa/icompras/clientes/service/EnderecoService.java
@@ -0,0 +1,32 @@
+package io.github.cursodsousa.icompras.clientes.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.cursodsousa.icompras.clientes.model.Endereco;
+import io.github.cursodsousa.icompras.clientes.repository.EnderecoRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+// Cria um construtor com todo os argumentos obrigatórios
+@RequiredArgsConstructor
+public class EnderecoService {
+
+    @Autowired
+    private final EnderecoRepository repository;
+
+    public Endereco salvar(Endereco endereco) {
+        return repository.save(endereco);
+    }
+
+    public Optional<Endereco> selecionarPorId(Long idendereco) {
+        return repository.findById(idendereco);
+    }
+
+    public List<Endereco> buscarPorIdCliente(Long idCliente) {
+        return repository.findByIdCliente(idCliente);
+    }
+}
