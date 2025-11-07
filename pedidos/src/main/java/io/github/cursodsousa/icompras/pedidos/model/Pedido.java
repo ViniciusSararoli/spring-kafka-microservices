9b7a4ab8aa42ccc54b80964bd97a136e53187c38
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-11-06 23:41:15 -0300

    Correção de importações e adicionando novas colunasd

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/model/Pedido.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/model/Pedido.java
@@ -2,6 +2,9 @@ package io.github.cursodsousa.icompras.pedidos.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import io.github.cursodsousa.icompras.pedidos.model.enums.StatusPedido;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,7 +12,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,7 +45,7 @@ public class Pedido {
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.ORDINAL) // Usar ordinal para salvar como inteiro no banco de dados
-    private Integer status;
+    private StatusPedido status;
 
     @Column(name = "total", nullable = false, precision = 16, scale = 2)
     private BigDecimal total;
@@ -50,4 +55,10 @@ public class Pedido {
 
     @Column(name = "codigo_rastreio", nullable = true, length = 300)
     private String codigoRastreio;
+
+    @Transient // Indica que este campo não será persistido no banco de dados
+    private DadosPagamento dadosPagamento;
+
+    @OneToMany(mappedBy = "idPedido") //   Indica o relacionamento com ItemPedido
+    private List<ItemPedido> itens;
 }
