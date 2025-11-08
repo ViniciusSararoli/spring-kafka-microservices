5d7c345af61b467956a6ef68e098ff55b4dd1758
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-11-08 00:22:21 -0300

    Refatorar método criar na classe PedidoService para simplificar a lógica de criação de pedidos e itens

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/service/PedidoService.java
@@ -1,19 +1,10 @@
 package io.github.cursodsousa.icompras.pedidos.service;
 
-//import java.math.BigDecimal;
-
 import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-
-//import io.github.cursodsousa.icompras.pedidos.controller.dto.ItemPedidoDTO;
-import io.github.cursodsousa.icompras.pedidos.controller.dto.NovoPedidoDTO;
-//import io.github.cursodsousa.icompras.pedidos.controller.mappers.ItemPedidoMapper;
-//import io.github.cursodsousa.icompras.pedidos.controller.mappers.PedidoMapper;
-//import io.github.cursodsousa.icompras.pedidos.model.ItemPedido;
 import io.github.cursodsousa.icompras.pedidos.model.Pedido;
 import io.github.cursodsousa.icompras.pedidos.repository.ItemPedidoRepository;
 import io.github.cursodsousa.icompras.pedidos.repository.PedidoRepository;
-import io.github.cursodsousa.icompras.pedidos.validator.PedidoValidator;
+// import io.github.cursodsousa.icompras.pedidos.validator.PedidoValidator;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -21,36 +12,14 @@ import lombok.RequiredArgsConstructor;
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
     private final ItemPedidoRepository itemPedidoRepository;
-    private final PedidoValidator pedidoValidator;
-    // private final PedidoMapper pedidoMapper;
-    // private final ItemPedidoMapper itemPedidoMapper;
+    // private final PedidoValidator pedidoValidator;
 
     // @Transactional
-    public Pedido criar(NovoPedidoDTO dto) {
-        // Validar o pedido
-        /*
-         * pedidoValidator.validar(dto);
-         * 
-         * // Criar o pedido principal
-         * Pedido pedido = pedidoMapper.map(dto);
-         * pedido = pedidoRepository.save(pedido);
-         * 
-         * // Criar e associar os itens
-         * BigDecimal total = BigDecimal.ZERO;
-         * for (ItemPedidoDTO itemDTO : dto.itens()) {
-         * ItemPedido item = itemPedidoMapper.toEntity(itemDTO);
-         * item.setIdPedido(pedido.getIdpedido());
-         * itemPedidoRepository.save(item);
-         * 
-         * // Atualizar o total
-         * total = total.add(item.getValor().multiply(new
-         * BigDecimal(item.getQuantidade())));
-         * }
-         * 
-         * // Atualizar o total do pedido
-         * pedido.setTotal(total);
-         * return pedidoRepository.save(pedido);
-         */
-        return null;
+    public Pedido criar(Pedido pedido) {
+        // Criar o pedido principal
+        pedidoRepository.save(pedido);
+        // Criar e associar os itens
+        itemPedidoRepository.saveAll(pedido.getItens());
+        return pedido;
     }
 }
