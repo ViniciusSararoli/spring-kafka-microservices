a29f34af8a1a1e9a0238ad76d39fde3ea676ffd9
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-11-08 00:18:39 -0300

    Refatorar mapeamento de pedidos e itens, melhorando a lógica de criação e totalização

--- pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/controller/mappers/ItemPedidoMapper.java
+++ pedidos/src/main/java/io/github/cursodsousa/icompras/pedidos/controller/mappers/ItemPedidoMapper.java
@@ -1,24 +1,14 @@
 package io.github.cursodsousa.icompras.pedidos.controller.mappers;
 
-import java.util.List;
-
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
-
 import io.github.cursodsousa.icompras.pedidos.controller.dto.ItemPedidoDTO;
 import io.github.cursodsousa.icompras.pedidos.model.ItemPedido;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ItemPedidoMapper {
-    
-    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+
     @Mapping(source = "produtoId", target = "idProduto")
     @Mapping(source = "precoUnitario", target = "valor")
-    @Mapping(target = "idPedido", ignore = true)
-    @Mapping(target = "iditem_pedido", ignore = true)
     ItemPedido map(ItemPedidoDTO dto);
-
-    List<ItemPedido> map(List<ItemPedidoDTO> dtos);
 }
