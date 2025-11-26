b3443ff6b2febd35eaafe77203b6c28e56fa6a5c
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-11-25 23:09:51 -0300

    Corrigir mapeamento da rota no para incluir o prefixo '/clientes'

--- pedidos/src/main/java/io/github/icompras/pedidos/client/ClientesClient.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/client/ClientesClient.java
@@ -1,5 +1,16 @@
 package io.github.icompras.pedidos.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import io.github.icompras.pedidos.client.representation.ClientesRepresentation;
+
+@FeignClient(name = "clientes-client", url = "${icompraspedidos.client.clientes.url}")
 public interface ClientesClient {
     
+    @GetMapping("/clientes/{idcliente}")
+    public ResponseEntity<ClientesRepresentation> obterDados(@PathVariable("idcliente") Long idcliente);
+
 }
