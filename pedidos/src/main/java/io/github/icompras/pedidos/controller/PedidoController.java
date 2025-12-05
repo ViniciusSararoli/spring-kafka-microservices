cc3faadcd88438465d69d8db62f944615814ec32
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-04 23:42:20 -0300

    Adicionar método para processar novo pagamento e melhorar formatação do código

--- pedidos/src/main/java/io/github/icompras/pedidos/controller/PedidoController.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/controller/PedidoController.java
@@ -1,6 +1,7 @@
 package io.github.icompras.pedidos.controller;
 
 import io.github.icompras.pedidos.model.ResponseData;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.icompras.pedidos.controller.dto.AdicionarNovoPagamentoDTO;
 import io.github.icompras.pedidos.controller.dto.NovoPedidoDTO;
 import io.github.icompras.pedidos.controller.mappers.PedidoMapper;
+import io.github.icompras.pedidos.model.exception.PedidoNaoEncontradoException;
 import io.github.icompras.pedidos.model.exception.ValidationException;
 import io.github.icompras.pedidos.service.PedidoService;
 import lombok.RequiredArgsConstructor;
@@ -27,7 +30,8 @@ public class PedidoController {
         try {
             var pedido = pedidoMapper.map(novoPedidoDTO);
             var novoPedido = pedidoService.criar(pedido);
-            var successResponse = new ResponseData.success("Pedido criado com sucesso", novoPedido.getIdpedido(), novoPedido.getTotal(), novoPedido.getStatus(), novoPedido.getDia());
+            var successResponse = new ResponseData.success("Pedido criado com sucesso", novoPedido.getIdpedido(),
+                    novoPedido.getTotal(), novoPedido.getStatus(), novoPedido.getDia());
             return ResponseEntity.ok(successResponse);
         } catch (ValidationException e) {
             var errorResponse = new ResponseData.error("Validation Error", e.getField(), e.getMessage());
@@ -39,5 +43,21 @@ public class PedidoController {
     public ResponseEntity<Object> obterDetalhes(@PathVariable Long idpedido) {
         var dadosPedido = pedidoService.selecionarPorId(idpedido);
         return ResponseEntity.ok(dadosPedido);
+    }
+
+    @PostMapping("/pagamento")
+    public ResponseEntity<Object> adicionarNovoPagamento(@RequestBody AdicionarNovoPagamentoDTO dto) {
+        try {
+            pedidoService.adicionarNovoPagamento(
+                    dto.pedidoId(),
+                    dto.detalhesPagamento(),
+                    dto.metodoPagamento());
+
+            return ResponseEntity.noContent().build();
+        } catch (PedidoNaoEncontradoException e) {
+            var errorResponse = new ResponseData.error("Pedido não encontrado", "pedidoId", e.getMessage());
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+
     }
 }
