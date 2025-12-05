190b6e175f0c9548099cf0d833a0e0c875a8c39d
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-04 23:41:39 -0300

    Remover importações não utilizadas no PagamentoController

--- pedidos/src/main/java/io/github/icompras/pedidos/controller/PagamentoController.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/controller/PagamentoController.java
@@ -1,7 +1,5 @@
 package io.github.icompras.pedidos.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
