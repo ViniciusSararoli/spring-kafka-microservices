c614f0d0d0e3438c0440cb11a35b2188d478d6c1
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-04 23:49:10 -0300

    Adicionar exceção personalizada PedidoNaoEncontradoException

---
+++ pedidos/src/main/java/io/github/icompras/pedidos/model/exception/PedidoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package io.github.icompras.pedidos.model.exception;
+
+public class PedidoNaoEncontradoException extends RuntimeException {
+    public PedidoNaoEncontradoException(String message) {
+        super(message);
+    }
+    
+}
