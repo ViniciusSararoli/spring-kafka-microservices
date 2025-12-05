d756a218504c3b787300ffd7d6c4f8d5823eaddd
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-04 23:44:40 -0300

    Adicionar anotações Getter e Setter à classe DadosPagamento

--- pedidos/src/main/java/io/github/icompras/pedidos/model/DadosPagamento.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/model/DadosPagamento.java
@@ -2,8 +2,12 @@ package io.github.icompras.pedidos.model;
 
 import io.github.icompras.pedidos.model.enums.MetodoPagamento;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class DadosPagamento {
     private MetodoPagamento metodoPagamento;
     private String detalhesPagamento;
