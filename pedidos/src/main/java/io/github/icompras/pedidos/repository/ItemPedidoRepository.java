84bef4a072d1b6813ce21f3a45c3e97edecc9c57
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-06 00:08:22 -0300

    Adicionar métodos para carregar dados do cliente e itens do pedido na classe PedidoService

--- pedidos/src/main/java/io/github/icompras/pedidos/repository/ItemPedidoRepository.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/repository/ItemPedidoRepository.java
@@ -1,11 +1,16 @@
 package io.github.icompras.pedidos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import io.github.icompras.pedidos.model.ItemPedido;
+import io.github.icompras.pedidos.model.Pedido;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
-
+    // Varificar se 'Pedido pedido' está igual dentro do método ItemPedido.java
+    List<ItemPedido> findByPedido(Pedido pedido);
+    
 }
--- pedidos/src/main/java/io/github/icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/service/PedidoService.java
@@ -1,12 +1,16 @@
 package io.github.icompras.pedidos.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import io.github.icompras.pedidos.client.ClientesClient;
+import io.github.icompras.pedidos.client.ProdutosClient;
 import io.github.icompras.pedidos.client.ServicoBancoClient;
 import io.github.icompras.pedidos.model.DadosPagamento;
+import io.github.icompras.pedidos.model.ItemPedido;
 import io.github.icompras.pedidos.model.Pedido;
 import io.github.icompras.pedidos.model.enums.StatusPedido;
 import io.github.icompras.pedidos.model.exception.PedidoNaoEncontradoException;
@@ -27,6 +31,9 @@ public class PedidoService {
     private final PedidoValidator pedidoValidator;
     // Cliente para interagir com o serviço de banco externo
     private final ServicoBancoClient servicoBancoClient;
+
+    private final ClientesClient clientesClient;
+    private final ProdutosClient produtosClient;
 
     /**
      * Ao finalizar a transação o pedido e seus itens serão salvos no banco de dados
@@ -80,7 +87,7 @@ public class PedidoService {
             log.error(mensagem);
             return;
         }
-        
+
         if (infoPedido.isPresent()) {
             var pedidoEncontrado = infoPedido.get();
             if (status) {
@@ -99,7 +106,7 @@ public class PedidoService {
 
         if (infoPedido.isEmpty()) {
             var mensagem = String.format("Pedido com ID %d não encontrado", pedidoId);
-             throw new PedidoNaoEncontradoException(mensagem);
+            throw new PedidoNaoEncontradoException(mensagem);
         }
 
         if (infoPedido.isPresent()) {
@@ -109,14 +116,45 @@ public class PedidoService {
             dadosPagamento.setDetalhesPagamento(detalhesPagamento);
             dadosPagamento.setMetodoPagamento(null);
 
-            pedidoEncontrado.setDadosPagamento(dadosPagamento);   
-            pedidoEncontrado.setStatus(StatusPedido.PAGAMENTO_REALIZADO);  
-            pedidoEncontrado.setObservacoes("Novo pagamento realizado com sucesso, pedido em preparação.");   
-            // Não é necessário chamar save explicitamente devido à anotação @Transactional   
+            pedidoEncontrado.setDadosPagamento(dadosPagamento);
+            pedidoEncontrado.setStatus(StatusPedido.PAGAMENTO_REALIZADO);
+            pedidoEncontrado.setObservacoes("Novo pagamento realizado com sucesso, pedido em preparação.");
+            // Não é necessário chamar save explicitamente devido à anotação @Transactional
             pedidoRepository.save(pedidoEncontrado);
 
             String pagamentoId = servicoBancoClient.solicitarPagamento(pedidoEncontrado);
             pedidoEncontrado.setIdPagamento(pagamentoId);
         }
+    }
+
+    public Optional<Pedido> carregarDadosPedid(Long idpedido) {
+        Optional<Pedido> pedidoOpt = pedidoRepository.findById(idpedido);
+        pedidoOpt.ifPresent(this::carregarDadosCliente);
+        pedidoOpt.ifPresent(this::carregarItensPedido);
+        return pedidoOpt;
+    }
+
+    private void carregarDadosCliente(Pedido pedido) {
+        // Lógica para carregar os dados do cliente associado ao pedido
+        // Isso pode envolver chamadas a outros serviços ou repositórios
+        Long idCliente = pedido.getIdCliente();
+        var resposta = clientesClient.obterDados(idCliente);
+        pedido.setDadosCliente(resposta.getBody());
+    }
+
+    private void carregarItensPedido(Pedido pedido) {
+        // Lógica para carregar os itens do pedido
+        // Isso pode envolver chamadas a outros serviços ou repositórios
+        List<ItemPedido> itensPedido = itemPedidoRepository.findByPedido(pedido);
+        pedido.setItens(itensPedido);
+        pedido.getItens().forEach(this::carregarDadosProduto);
+    }
+
+    @SuppressWarnings("null")
+    private void carregarDadosProduto(ItemPedido itemPedido) {
+        Long idProduto = itemPedido.getIdProduto();
+        var resposta = produtosClient.obterDados(idProduto);
+        // Metodo para setar as propriedades do produto no itemPedido
+        itemPedido.setNomeProduto(resposta.getBody().nome());
     }
 }
