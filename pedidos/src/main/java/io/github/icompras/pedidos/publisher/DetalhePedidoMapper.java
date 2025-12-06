ca07171229fd94a2fe4b39f9c7e7b9e5663913af
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2025-12-05 23:29:35 -0300

    Adicionar campo dadosCliente à classe Pedido e atualizar mapeamento em DetalhePedidoMapper para incluir informações do cliente na representação do pedido

--- pedidos/src/main/java/io/github/icompras/pedidos/publisher/DetalhePedidoMapper.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/publisher/DetalhePedidoMapper.java
@@ -1,5 +1,28 @@
 package io.github.icompras.pedidos.publisher;
 
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import io.github.icompras.pedidos.model.Pedido;
+import io.github.icompras.pedidos.publisher.representarion.DetalhePedidoRepresentation;
+
+@Mapper(componentModel = "spring", uses = { DetalheItemPedidoMapper.class })
 public interface DetalhePedidoMapper {
+    // source = origem = Pedido, target = destino = DetalhePedidoRepresentation
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(source = "idpedido", target = "pedidoId")
+    @Mapping(source = "idCliente", target = "clienteId")
+    @Mapping(expression = "java(pedido.getDia() != null ? java.time.format.DateTimeFormatter.ofPattern(\"yyyy-MM-dd HH:mm:ss\").format(pedido.getDia()) : null)", target = "dia")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "total", target = "total")
+    // Equivalente aos dados do ClientesRepresentation.java
+    @Mapping(source = "dadosCliente.cpfCnpj", target = "cpfCnpj")
+    @Mapping(source = "dadosCliente.email", target = "email")
+    @Mapping(source = "dadosCliente.telefone", target = "telefone")
     
+    @Mapping(target = "itens", source = "itens")
+
+    DetalhePedidoRepresentation map(Pedido pedido);
 }
--- pedidos/src/main/java/io/github/icompras/pedidos/publisher/representarion/DetalhePedidoRepresentation.java
+++ pedidos/src/main/java/io/github/icompras/pedidos/publisher/representarion/DetalhePedidoRepresentation.java
@@ -1,12 +1,13 @@
 package io.github.icompras.pedidos.publisher.representarion;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public record DetalhePedidoRepresentation(
     Long pedidoId,
     Long clienteId,
-    String nomecliente,
-    String cpf_cnpj,
+    String nomeCliente,
+    String cpfCnpj,
     String logradouro,
     String numero,
     String bairro,
@@ -15,7 +16,7 @@ public record DetalhePedidoRepresentation(
     String status,
     String dia,
     BigDecimal total,
-    DetalheItemPedidoRepresentarion[] itens
+    List<DetalheItemPedidoRepresentation> itens
 ) {
     
 }
